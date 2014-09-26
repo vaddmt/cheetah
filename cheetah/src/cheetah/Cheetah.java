84090b54360178d6c1cac5e7fed6f786f8cde68b
vaddmt <vad-dmt@rambler.ru>
2014-09-27 00:46:18 +0400

    one more comment

--- cheetah/src/cheetah/Cheetah.java
+++ cheetah/src/cheetah/Cheetah.java
@@ -6,6 +6,7 @@ import cheetahGUI.*;
 public class Cheetah {
     public static void main(String[] args) {
 //my comment
+//yea, i've got it!
         MainFrame m = new MainFrame("Cheetah", 384, 640);
     }
 }
