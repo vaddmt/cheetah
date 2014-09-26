9ffe693d16389b47201562ccc846bf5e30f1325f
vaddmt <vad-dmt@rambler.ru>
2014-09-26 21:31:47 +0400

    first commit
    creating new empty project

---
+++ cheetah/src/cheetah/Cheetah.java
@@ -0,0 +1,10 @@
+package cheetah;
+
+import javax.swing.*;
+import cheetahGUI.*;
+
+public class Cheetah {
+    public static void main(String[] args) {
+        MainFrame m = new MainFrame("Cheetah", 384, 640);
+    }
+}
